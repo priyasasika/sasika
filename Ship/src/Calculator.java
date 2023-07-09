1a658db14e08d54e42cef0a7e7b5cef2c21031ee
sasi <skarimafeb@gmail.com>
2023-07-09 19:30:37 -0400

    second commit

---
+++ Ship/src/Calculator.java
@@ -0,0 +1,19 @@
+
+public class Calculator {
+int n1=5,n2=2;
+public void add() {
+	System.out.println("sum is:"+(n1+n2));
+}
+public void mul() {
+	System.out.println("mul is:"+(n1*n2));
+}
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Calculator cal=new Calculator();
+cal.add();
+	}
+
+}
